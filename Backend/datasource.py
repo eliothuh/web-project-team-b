f3466b9d27bb5959d1ef99fc22feb3c1a1a72b17
Lab User <labuser@libr451-08r.its.carleton.edu>
2019-10-30 15:50:11 -0500

    edited datasource

--- Backend/datasource.py
+++ Backend/datasource.py
@@ -68,23 +68,6 @@ class DataSource:
                 "Wyoming" : "WY",
                 }
 
-    def connect(user, password):
-        '''
-        Establishes a connection to the database with the following credentials:
-		user - username, which is also the name of the database
-		password - the password for this database on perlman
-
-        Returns: a database connection.
-
-        Note: exits if a connection cannot be established.
-        '''
-        try:
-            connection = psycopg2.connect(database=user, user=user, password=password)
-        except Exception as e:
-            print("Connection error: ", e)
-            exit()
-            return connection
-
 
     def USAAllYearsQuery(self):
         '''
@@ -273,6 +256,25 @@ class DataSource:
         if (startYear < 1999 or endYear > 2017 or startYear > endYear):
             print("invalid year range")
             exit()
+            
+            
+def connect(user, password):
+     '''
+        Establishes a connection to the database with the following credentials:
+		user - username, which is also the name of the database
+		password - the password for this database on perlman
+
+        Returns: a database connection.
+
+        Note: exits if a connection cannot be established.
+        '''
+    try:
+        connection = psycopg2.connect(database=user, user=user, password=password)
+    except Exception as e:
+        print("Connection error: ", e)
+        exit()
+    return connection
+
 
 def main():
 
@@ -280,9 +282,9 @@ def main():
     password = getpass.getpass()
 
     # Connect to the database
-    connection = psycopg2.connect(database=user, user=user, password=password)
+    #connection = psycopg2.connect(database=user, user=user, password=password)
 
-    datasource = DataSource(connection)
+    datasource = DataSource(connect(user, password))
 
     # Execute a simple query: how many earthquakes above the specified magnitude are there in the data?
     results = datasource.stateSingleYearQuery(2000, "Florida")
@@ -292,7 +294,7 @@ def main():
     results = datasource.countyQuery(2000, 2020, "Los Angeles County, CA")
 
     if results is not None:
-        print("Query results: ")
+        print("Query results: ") 
         for item in results:
             print(item)
 
