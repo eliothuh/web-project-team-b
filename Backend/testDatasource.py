5ef76671e23e31eb56034515c819f63e57ae1e95
Lab User <labuser@libr451-08r.its.carleton.edu>
2019-10-30 17:30:22 -0500

    fixed tests

--- Backend/testDatasource.py
+++ Backend/testDatasource.py
@@ -9,13 +9,13 @@ class DataSourceTester(unittest.TestCase):
 		self.dataSource = DataSource(connection)
 		
 	def test_check_incompatible_range(self, startYear, endYear):
-		assertRaises(ValueError, self.dataSource.checkValidRange, 2000, 1999)
-		assertRaises(ValueError, self.dataSource.checkValidRange, 1960, 1999)
-		assertRaises(ValueError, self.dataSource.checkValidRange, 2017, 2020)
-		assertRaises(TypeError, self.dataSource.checkValidRange, 'apple', 'banana')
+		self.assertRaises(ValueError, self.dataSource.checkValidRange, (2000, 1999))
+		self.assertRaises(ValueError, self.dataSource.checkValidRange, (1960, 1999))
+		self.assertRaises(ValueError, self.dataSource.checkValidRange, (2017, 2020))
+		self.assertRaises(TypeError, self.dataSource.checkValidRange, ('apple', 'banana'))
 	
 	def test_check_compatible_range(self, startYear, endYear):
-		assertTrue(self.dataSource.checkValidRange(2000, 2002))
+		self.assertTrue(self.dataSource.checkValidRange(2000, 2002))
 		
 if __name__ == '__main__':
 	unittest.main()
