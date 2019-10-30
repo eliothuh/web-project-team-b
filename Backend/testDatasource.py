5673753914f188704da9a23748b5ec431af93c9b
Lab User <labuser@libr451-08r.its.carleton.edu>
2019-10-30 17:15:52 -0500

    fixed tests

--- Backend/testDatasource.py
+++ Backend/testDatasource.py
@@ -8,13 +8,13 @@ class DataSourceTester(unittest.TestCase):
 		connection = psycopg2.connect("huhe", "huhe", "tree695eye")
 		self.dataSource = DataSource(connection)
 		
-	def check_incompatible_range(self, startYear, endYear):
+	def test_check_incompatible_range(self, startYear, endYear):
 		assertRaises(ValueError, self.dataSource.checkValidRange, (2000, 1999))
 		assertRaises(ValueError, self.dataSource.checkValidRange, (1960, 1999))
 		assertRaises(ValueError, self.dataSource.checkValidRange, (2017, 2020))
 		assertRaises(TypeError, self.dataSource.checkValidRange, ('apple', 'banana'))
 	
-	def check_compatible_range(self, startYear, endYear):
+	def test_check_compatible_range(self, startYear, endYear):
 		assertTrue(self.dataSource.checkValidRange(2000, 2002))
 		
 if __name__ == '__main__':
