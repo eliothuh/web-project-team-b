1606b50eb0354f2118130ba4c36b266e11e9563a
Lab User <labuser@wcc138-07r.build.carleton.edu>
2019-11-11 16:53:16 -0600

    edited flask

--- Backend/webapp.py
+++ Backend/webapp.py
@@ -144,9 +144,9 @@ def getStateQueryResults():
 		state = request.form.get('state')
 		dataTable = getStateData(start, end, state)
 
-		return render_template("Results.html", dataTable["stateCrudeRate"], dataTable["nationalCrudeRate"])
+		return render_template("Results2.html", dataTable["stateCrudeRate"], dataTable["nationalCrudeRate"])
 	
-	return render_template("Results.html")
+	return render_template("HomePage2.html")
 
 @app.route('/home/')
 def boring():
