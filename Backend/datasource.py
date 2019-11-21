5dd35233e0272d4d79b21d72515c5260a5b9f219
Lab User <labuser@libr451-17r.its.carleton.edu>
2019-11-21 15:57:38 -0600

    changed how states are handled

--- Backend/datasource.py
+++ Backend/datasource.py
@@ -241,7 +241,7 @@ class DataSource:
 
 		try:
 			cursor = self.connection.cursor()
-			query = f"SELECT * FROM states{year} WHERE lower(statename) = '{state}'"
+			query = f"SELECT * FROM states{year} WHERE statename = '{state}'"
 			cursor.execute(query)
 			results = cursor.fetchall()
 
