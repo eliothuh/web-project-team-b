f7e7fed75614285acbd4550e8261a4970e6cff10
Eliot <huhe@carleton.edu>
2019-11-24 21:18:18 -0600

    updated flask

--- Backend/webapp.py
+++ Backend/webapp.py
@@ -125,7 +125,7 @@ def formatJavascriptString(list, variableName):
 	a variable with our specified name in our Javascript file.
 	'''
 	javascriptString = "var " + variableName + " = "
-	javascriptString += ''.join(list)
+	javascriptString += ",".join( [ str(element) for element in list ] )
 	print("no errors yet!")
 	return javascriptString
 
