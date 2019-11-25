246e179d7278a2297a2bafa50a91acec16ec7777
Eliot <huhe@carleton.edu>
2019-11-24 20:57:49 -0600

    updated flask

--- Backend/webapp.py
+++ Backend/webapp.py
@@ -524,11 +524,11 @@ def getMapQueryResults():
 										endYear = end)
 
 
-if __name__ == '__main__':
+"""if __name__ == '__main__':
 	if len(sys.argv) != 3:
 		print('Usage: {0} host port'.format(sys.argv[0]), file=sys.stderr)
 		exit()
 
 	host = sys.argv[1]
 	port = sys.argv[2]
-	app.run(host=host, port=port)
+	app.run(host=host, port=port)"""
